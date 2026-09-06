033b09404f4b2bd3848f37882b0ae51812739bff
ishagupta35318-lab <ishagupta35318@gmail.com>
2026-09-06 20:56:36 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Thank you for visiting my profile! 😊";
+}
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
